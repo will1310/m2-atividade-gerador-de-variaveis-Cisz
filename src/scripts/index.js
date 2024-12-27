65ce8e738ea2122e8178edb3d3082dafa1952db0
Cisz <cisz13h@gmail.com>
2024-12-27 10:02:03 -0300

    completo

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,9 @@
-const inputsColor = [...document.querySelectorAll("input[type=color]")];
+const colorInputs = [...document.querySelectorAll("input[type=color]")];
+const fontSizeInput = document.querySelector("#font-scale");
+const copyButton = document.querySelector(".form__buttons--copy");
+const saveButton = document.querySelector(".form__buttons--save");
 
-const colors = [
+let colors = [
   "#5f3dc4",
   "#7048e8",
   "#7950f2",
@@ -16,5 +19,82 @@ const colors = [
 ];
 
 colors.forEach((style, index) => {
-  inputsColor[index].value = style;
+  colorInputs[index].value = style;
 });
+
+const cssColorVariables = [];
+const cssFontSizeVariables = [];
+let cssPureSizes = "";
+
+const createColorVariables = () => {
+  colors = []
+  colorInputs.forEach((input) => {
+    const colorVar = `--color-${input.id}: ${input.value};`;
+    cssColorVariables.push(colorVar);
+    colors.push(input.value);
+  });
+
+  return cssColorVariables;
+};
+
+const createFontSizeVariables = () => {
+
+  if (!fontSizeInput.value.trim()) {
+    return []
+  }
+
+  const fontSizeValue = fontSizeInput.value
+    .replace(/\s/g, "")
+    .split(",")
+    .map((value) => Number(value) / 16);
+
+  fontSizeValue.forEach((value, index) => {
+    const fontVar = `--font-size-${index + 1}: ${value}rem;`;
+    cssFontSizeVariables.push(fontVar);
+    cssPureSizes = fontSizeInput.value.replace(/\s/g, "");
+  });
+
+  return cssFontSizeVariables;
+};
+
+copyButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  copyButton.innerText = "Copied!";
+
+  setTimeout(() => {
+    copyButton.innerText = "Copy Styles";
+  }, 3000);
+
+  navigator.clipboard.writeText([
+    ...createColorVariables(),
+    ...createFontSizeVariables(),
+  ].join(""));
+});
+
+saveButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  saveButton.innerText = "Saved!";
+
+  setTimeout(() => {
+    saveButton.innerText = "Save Styles";
+  }, 3000);
+
+  createColorVariables();
+  createFontSizeVariables();
+
+  localStorage.setItem("@colors", JSON.stringify(colors));
+  localStorage.setItem("@fonts", JSON.stringify(cssPureSizes));
+});
+
+if (localStorage.getItem("@colors")) {
+  colors = JSON.parse(localStorage.getItem("@colors"));
+  colors.forEach((style, index) => {
+    colorInputs[index].value = style;
+  });
+}
+
+if (localStorage.getItem("@fonts")) {
+  fontSizeInput.value = JSON.parse(localStorage.getItem("@fonts"))
+}
